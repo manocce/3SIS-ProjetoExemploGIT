2b1da7d7621365eb0a4a5d5c779eb59cbc9bf504
manocce <73118585+manocce@users.noreply.github.com>
2024-04-04 20:30:50 -0300

    Atualizando o prog Calculadora

--- PastaFontes/ExercicioExtraJUNITSolucao/ProjetoElevador/src/FontesAppElevador/Calculadora.java
+++ PastaFontes/ExercicioExtraJUNITSolucao/ProjetoElevador/src/FontesAppElevador/Calculadora.java
@@ -4,27 +4,27 @@ package FontesAppElevador;
 public class Calculadora {
 
 	//Atributo de resultado de cálculo
-	double resultado;
+	double resultadoo;
 	
 	//Getters & Setters
 	public void setResultado(double valor) {
-		this.resultado = valor;
+		this.resultadoo = valor;
 	}
 	
 	public double getResultado() {
-		return this.resultado;
+		return this.resultadoo;
 	}
 	
 	//Método de soma de dois fatores
 	public double somar(double valor1, double valor2) {
-		this.resultado = valor1 + valor2;
-		return this.resultado;
+		this.resultadoo = valor1 + valor2;
+		return this.resultadoo;
 	}
 	
 	//Método de subtração de dois fatores
 	public double subtrair(double valor1, double valor2) {
-		this.resultado = valor1 - valor2;
-		return this.resultado;
+		this.resultadoo = valor1 - valor2;
+		return this.resultadoo;
 	}
 
 }
